9ad7af6b56acf5780ff27389a1c31bcd587025bc
Gergo Nagy <guzmop@gmail.com>
2017-08-01 22:24:01 +0100

    add journal

---
+++ Journal.java
@@ -0,0 +1,14 @@
+import java.util.HashMap;
+
+public class Journal {
+
+  public static void main(String[] args) {
+    
+    HashMap<String, Integer> photos = new HashMap<String, Integer>();
+
+    photos.put("Mon", 10);
+    photos.put("Tue", 23);
+    photos.put("Wen", 8);
+
+  }
+}
