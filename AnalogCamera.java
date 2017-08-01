7d442624fcb0b585b93fa75c86c5e2a1b2284c44
Gergo Nagy <guzmop@gmail.com>
2017-08-01 16:18:01 +0100

    create digital/analog class

---
+++ AnalogCamera.java
@@ -0,0 +1,6 @@
+public class AnalogCamera implements Printable {
+
+  public String printDetails(){
+    return "40mm film with a 45-90mm lens";
+  }
+}
---
+++ DigitalCamera.java
@@ -0,0 +1,6 @@
+public class DigitalCamera implements Printable {
+
+  public String printDetails(){
+    return "Nikon 16mpx with 50mm lens";
+  }
+}
